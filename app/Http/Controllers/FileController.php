3167533efacbdfb2bd304998e169a1fb39befc7e
Chadi <chadimangled@gmail.com>
2025-03-02 13:25:13 +0100

    auth

---
+++ app/Http/Controllers/FileController.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Http\Request;
+use App\Models\File;
+use Illuminate\Support\Facades\Storage;
+
+class FileController extends Controller
+{
+    public function store(Request $request)
+    {
+        $request->validate([
+            'file' => 'required|max:2048'
+        ]);
+
+        if ($request->file('file')) {
+            $file = $request->file('file');
+            $path = $file->store('uploads', 'public');
+
+            $savedFile = File::create([
+                'name' => $file->getClientOriginalName(),
+                'path' => $path
+            ]);
+
+            return response()->json([
+                'message' => 'Fichier enregistré avec succès',
+                'file' => $savedFile
+            ], 201);
+        }
+
+        return response()->json(['error' => 'Erreur lors de l\'upload'], 400);
+    }
+
+    public function download(File $file)
+    {
+        return Storage::download('public/' . $file->path);
+    }
+}
